39454d0e3913be8f365227434b1c2af52d7dd2d2
Satriotol <satriotol69@gmail.com>
2019-07-15 23:50:11 +0700

    article kurang href

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -3,122 +3,130 @@
 <style>
     .article {
         margin-top: 66px;
-        padding-top: 100px;
-        ;
+        padding-top: 30px;
         padding-bottom: 100px;
     }
 
+    @media screen and (max-width: 700px) {
+        .article {
+            margin-top: 0px;
+            padding-top: 66px;
+            padding-bottom: 10px;
+        }
+    }
+
     .module {
-        background-color: white;
-        padding
+        border-style: solid;
+        border-color: #E6E6EA;
+        border-radius: 10px;
+        border-width: 1px;
+        background-color: #EFEFF1;
+        padding-top: 75px;
+        padding-bottom: 75px;
+    }
+    .ot-circle {
+        font-size: 75px;
+        color: white;
+        transition: all .3s;
+    }
+    .ot-circle:hover {
+        color: #fed136;
+    }
+
+    @media screen and (max-width: 700px) {
+        .jarak {
+            margin-bottom: 40px;
+        }
     }
 
 </style>
 
 <body>
     <div class="latar-lihat-berita container-fluid">
-        <div class="article container">
+        <div class="article container-fluid">
             <div class="row">
-                <div class="col-md-3">
-                    <div class="module">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                            tempora
-                            eaque
-                            eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                            exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                            Quisquam
-                            tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                            minima
-                            iure
-                            dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
             </div>
             <br>
             <div class="row">
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo illum omnis quo pariatur
-                        tempora
-                        eaque
-                        eos dolores optio quos consequuntur, modi corrupti odit provident incidunt id dignissimos,
-                        exercitationem repellat saepe. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Quisquam
-                        tempora veniam voluptates deleniti animi ducimus enim blanditiis libero atque deserunt
-                        minima
-                        iure
-                        dolor magnam provident, ex cumque. Dolor, delectus alias.</p>
+                <div class="col-md-3 jarak">
+                    <div class="module text-center">
+                        <i class="fa fa-briefcase color1 ot-circle">
+                        </i>
+                        <br>
+                        <p>
+                            PRESS RELEASE
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
